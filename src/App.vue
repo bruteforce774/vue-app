59bdb75a17099c5de1c862f17ae0e62832178efd
bruteforce774 <danielaw@getacademy.no>
2025-10-07 14:25:05 +0200

    use ref/reactive

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
-<script setup></script>
+<script setup>
+import { ref, reactive } from 'vue'
+
+const message = ref('Hi!')
+
+const counter = reactive({
+  count: 0
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>{{ message }}</h1>
+  <button @click="counter.count++">Count is: {{ counter.count }}</button>
+  &nbsp;<button @click="counter.count = 0">Reset</button>
 </template>
 
 <style scoped></style>
